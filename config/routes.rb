90d4bcfd1cf1da9de48cf7e4e31bc5c7a8c78830
Clayton Siby <claytonsiby@gmail.com>
2022-04-17 15:53:37 +0200

    Update the routes for creating sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  devise_for :users
-  resources :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+  # resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
